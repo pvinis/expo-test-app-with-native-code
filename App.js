d25f7229b52c60a1c83e49181be80ec6466673ea
Pavlos Vinieratos <pvinis@gmail.com>
2022-11-08 18:22:23 +0000

    ahaaa

--- App.js
+++ App.js
@@ -1,12 +1,18 @@
 import "expo-dev-client";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { NativeModules, Button, StyleSheet, Text, View } from "react-native";
+
+const { MAYCalendarModule } = NativeModules;
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to asaaaaastart working on your app!</Text>
       <StatusBar style="auto" />
+      <Button
+        title="print"
+        onPress={() => MAYCalendarModule.createCalendarEvent("yep")}
+      />
     </View>
   );
 }
---
+++ ios/MAYCalendarModule.m
@@ -0,0 +1,13 @@
+#import "MAYCalendarModule.h"
+
+
+@implementation MAYCalendarModule
+
+RCT_EXPORT_MODULE();
+
+RCT_EXPORT_METHOD(createCalendarEvent:(NSString *)name)
+{
+  NSLog(@"wow!");
+}
+
+@end
